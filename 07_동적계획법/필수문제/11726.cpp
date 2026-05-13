33e62e3e1fc4d4af4ea4dac9551e1ceb45841111
naeun0825 <nayun0825@naver.com>
2026-05-13 19:27:57 +0900

    [동적계획법] 5월 13일 - Update

--- 07_동적계획법/필수문제/11726.cpp
+++ 07_동적계획법/필수문제/11726.cpp
@@ -12,10 +12,10 @@ int main() {
 	arr[2] = 2;
 
 	for (int i = 3; i <= n; i++) {
-		arr[i] = arr[i - 1] + arr[i - 2];
+		arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
 	}
 
-	cout << arr[n] % 10007 << "\n";
+	cout << arr[n] << "\n";
 
 	return 0;
 }
